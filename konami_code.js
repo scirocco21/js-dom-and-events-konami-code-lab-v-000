d9fb48f45b068728b2faf3d1b1b4615247329c54
Seb Gertz <sebgertz@gmail.com>
2018-06-04 19:52:59 +0000

    code should work, weird mocha error

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+function init(){
+
   // Write your JavaScript code inside the init() function
 
+  document.body.addEventListener('keydown',  function(e) {
+    let index = 0;
+    const key = parseInt(e.key || e.which || e.char);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        window.alert("Hurray!");
+        index = 0;
+      }
+  } else {
+    index = 0;
+    }
+  });
 }
